19b98b731f0f491717a3b45b4a8496607d8d734b
aidiwe <37834624+aidiwe@users.noreply.github.com>
2022-01-31 22:40:46 +0300

    Update NewToken.sol

--- NewToken.sol
+++ NewToken.sol
@@ -5,9 +5,9 @@ import "./ERC20Standard.sol";
 contract NewToken is ERC20Standard {
 	constructor() public {
 		totalSupply = 123;
-		name = "Best ever coin";
+		name = "BRITVA";
 		decimals = 4;
-		symbol = "BEC";
+		symbol = "BRITVA";
 		version = "1.0";
 		balances[msg.sender] = totalSupply;
 	}
